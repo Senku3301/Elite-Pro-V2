b503c400e3efada625f6b895af1d3d2a00f133e3
Senku3301 <adjomlaafiwa@gmail.com>
2024-09-25 14:28:44 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,19 +11,19 @@ global.socialm = "GitHub: Elite-MD" //ur github or insta name
 global.location = "Port Harcourt, Nigeria" //ur location
 
 //new
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.botname = 'PARFUNO-BOT' //ur bot name
+global.ownernumber = '22899952910' //ur owner number
+global.ownername = 'PARFUNO-BOT' //ur owner name
 global.websitex = "https://youtu.be/@elite-pro-empire"
 global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
-global.themeemoji = '👨🏼‍💻'
+global.themeemoji = '☯️'
 global.wm = "ELITE-PRO-V2"
 global.botscript = 'https://github.com/Elite-MD/ELITE-PRO-V2' //script link
 global.packname = "Sticker By"
 global.author = "ELITE-PRO-V2"
 global.creator = "2348109263390@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["2348109263390"] // Premium User
+global.premium = ["22899952910"] // Premium User
 global.hituet = 0
 
 //bot sett
